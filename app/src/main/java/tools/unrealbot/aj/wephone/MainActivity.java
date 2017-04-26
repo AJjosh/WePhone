25b8e6cf0995db0133ebf48e7391b71e7c32cab5
AJjosh <joshi.ashok11@outlook.com>
2017-04-26 15:22:38 +0800

    Updating version 0.1

--- app/src/main/java/tools/unrealbot/aj/wephone/MainActivity.java
+++ app/src/main/java/tools/unrealbot/aj/wephone/MainActivity.java
@@ -1,5 +1,9 @@
 package tools.unrealbot.aj.wephone;
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,11 +14,30 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //REGISTER A BROADCAST RECEIVER
+        MyReceiver myrcr = new MyReceiver();
+        IntentFilter itf = new IntentFilter();
+        itf.addAction("fromSDKManager");
         MySDKManager MySdkInstance = new MySDKManager(this);
         MySdkInstance.CreateClientConfig();
         Log.i(myTag, "Created Client Config");
         MySdkInstance.createUserConfiguration("80813","123456", "lab.com",false,"135.60.51.130",5060);
         MySdkInstance.registerUser();
 
+
+
+    }
+
+    //Implement a Broadcast Receiver to Receive the Register Event
+    private class MyReceiver extends BroadcastReceiver{
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String message = intent.getStringExtra("message");
+            Log.i(myTag, getClass().getCanonicalName()+ ": Got event: "+message);
+            //IF LOOP TO TAKE VARIOUS ACTION ON WHATEVER BE THE MESSAGE
+
+
+        }
     }
 }
--- app/src/main/java/tools/unrealbot/aj/wephone/MySDKManager.java
+++ app/src/main/java/tools/unrealbot/aj/wephone/MySDKManager.java
@@ -7,8 +7,10 @@ package tools.unrealbot.aj.wephone;
 import android.app.Activity;
 //import android.app.Application;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Build;
+import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
 import com.avaya.clientservices.call.Call;
@@ -48,6 +50,53 @@ public class MySDKManager implements ClientListener, CredentialProvider, UserReg
     private String password =null;
     private String domain =null;
 
+    //Define Broadcast Strings, They wil b eused to Broadcast events, all may not be used
+    private String AUTH_CHALLENGE_RECEIVED = "onAuthenticationChallenge";
+    private String CRED_ACCEPTED="onCredentialAccepted";
+    private String AUTH_CHALLENGE_CANCELLED="nAuthenticationChallengeCancelled";
+    private String CLIENT_SHUTDOWN="onClientShutdown";
+    private String CLIENT_USER_CREATED="onClientUserCreated";
+    private String CLIENT_USER_REMOVED ="onClientUserRemoved";
+    private String REGISTER_INPROGRESS ="onUserRegistrationInProgress";
+    private String REGISTER_SUCCESS="onUserRegistrationSuccessful";
+    private String REGISTER_FAIL="onUserRegistrationFailed";
+    private String REGISTER_ALL_SUCCESS ="onUserAllRegistrationsSuccessful";
+    private String REGISTER_ALL_FAIL= "onUserAllRegistrationsFailed";
+    private String UNREGISTER_INPROGRESS ="onUserUnregistrationInProgress";
+    private String UNREGISTER_SUCCESS = "onUserUnregistrationSuccessful";
+    private String UNREGISTER_FAIL ="onUserUnregistrationFailed";
+    private String UNREGISTER_COMPLETE= "onUserUnregistrationComplete";
+    private String CALL_START = "onCallStarted";
+    private String REMOTE_CALL_ALERTING = "onCallRemoteAlerting";
+    private String CALL_REDIRECTED ="onCallRedirected";
+    private String CALL_QUEUED = "onCallQueued";
+    private String CALL_ESTABLISHED = "onCallEstablished";
+    private String REMOTE_ADDRESS_CHANGE ="onCallRemoteAddressChanged";
+    private String CALL_HELD ="onCallHeld";
+    private String CALL_UNHOLD ="onCallUnheld";
+    private String CALL_HELD_REMOTELY ="onCallHeldRemotely";
+    private String CALL_UNHELD_REMOTELY ="onCallUnheldRemotely";
+    private String CALL_JOINED="onCallJoined";
+    private String CALL_END ="onCallEnded";
+    private String CALL_FAILED ="onCallFailed";
+    private String CALL_DENIED = "onCallDenied";
+    private String CALL_IGNORED = "onCallIgnored";
+    private String CALL_AUDIOMUTE_CHANGED="onCallAudioMuteStatusChanged";
+    private String CALL_CONF_STATUS_CHANGED="onCallConferenceStatusChanged";
+    private String CALL_CAPABILITIES_CHANGED = "onCallCapabilitiesChanged";
+    private String CALL_SERVICE_AVAIL="onCallServiceAvailable";
+    private String CALL_SERVICE_UNAVAIL= "onCallServiceUnavailable";
+
+
+
+
+
+
+
+
+
+
+
 
 
     public MySDKManager(Activity activity){
@@ -78,7 +127,7 @@ public class MySDKManager implements ClientListener, CredentialProvider, UserReg
         }
         else {
             userInstanceId=PersistUUID();
-            Log.i(myTag, "Created a new UUID as its not present with a value of: "+userInstanceId);
+            Log.i(myTag, this.getClass().getCanonicalName()+"Created a new UUID as its not present with a value of: "+userInstanceId);
         }
         MyClientConfig.setUserAgentName(userInstanceId);
         myClient = new Client(MyClientConfig, activity.getApplication(),this );
@@ -89,25 +138,25 @@ public class MySDKManager implements ClientListener, CredentialProvider, UserReg
         this.userId=userId;
         this.password=password;
         this.domain=domain;
-        Log.i(myTag, "Trying to create the UserConfiguration object");
+        Log.i(myTag, this.getClass().getCanonicalName()+"Trying to create the UserConfiguration object");
         userConfig = new UserConfiguration();
-        Log.i(myTag, "Retrieving the SIPUserConfiguration object form the UserConfiguration object");
+        Log.i(myTag, this.getClass().getCanonicalName()+"Retrieving the SIPUserConfiguration object form the UserConfiguration object");
         SIPUserConfiguration sipUserConfig = userConfig.getSIPUserConfiguration();
         sipUserConfig.setEnabled(true);
         sipUserConfig.setUserId(userId);
         sipUserConfig.setDomain(domain);
-        Log.i(myTag,"Setting a Signaling server with TLS/TCP");
+        Log.i(myTag,this.getClass().getCanonicalName()+"Setting a Signaling server with TLS/TCP");
         SignalingServer.TransportType tType = useTls ? SignalingServer.TransportType.TLS : SignalingServer.TransportType.TCP;
         SignalingServer.FailbackPolicy myPolicy = SignalingServer.FailbackPolicy.AUTOMATIC;
         SignalingServer mySigServer =  new SignalingServer(tType, serverName, serverPort, myPolicy);
         sipUserConfig.setConnectionPolicy(new ConnectionPolicy(mySigServer));
-        Log.i(myTag,"Successfully set the ConnectionPolicy object");
-        Log.i(myTag,"Setting the Credentials using the CredentialProvider method");
+        Log.i(myTag, this.getClass().getCanonicalName()+"Successfully set the ConnectionPolicy object");
+        Log.i(myTag, this.getClass().getCanonicalName()+"Setting the Credentials using the CredentialProvider method");
         sipUserConfig.setCredentialProvider(this);
-        Log.i(myTag, "Finished the SIP USer configuration, saving it to the UserConfiguration object");
+        Log.i(myTag, this.getClass().getCanonicalName()+"Finished the SIP USer configuration, saving it to the UserConfiguration object");
         userConfig.setSIPUserConfiguration(sipUserConfig);
-        Log.i(myTag, " User configuration completed and saved in the UserConfiguration object ");
-        Log.i(myTag, "Going to create the User Object now");
+        Log.i(myTag, this.getClass().getCanonicalName()+" User configuration completed and saved in the UserConfiguration object ");
+        Log.i(myTag, this.getClass().getCanonicalName()+"Going to create the User Object now");
     }
 
     public void registerUser(){
@@ -122,7 +171,7 @@ public class MySDKManager implements ClientListener, CredentialProvider, UserReg
 
             @Override
             public void onError(UserCreatedException e) {
-                Log.i(myTag, "Failed to create the user, exception follows");
+                Log.i(myTag, this.getClass().getCanonicalName()+"Failed to create the user, exception follows");
                 e.printStackTrace();
 
             }
@@ -141,7 +190,7 @@ public class MySDKManager implements ClientListener, CredentialProvider, UserReg
     private String PersistUUID() {
         String userInstanceId =null;
         userInstanceId= UUID.randomUUID().toString();
-        Log.i(myTag, "Generated a UUID for this client, with a value of : "+userInstanceId);
+        Log.i(myTag, this.getClass().getCanonicalName()+"Generated a UUID for this client, with a value of : "+userInstanceId);
         SharedPreferences sharedPref = activity.getApplication().getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor= sharedPref.edit();
         editor.putString("UUID", userInstanceId );
@@ -153,23 +202,25 @@ public class MySDKManager implements ClientListener, CredentialProvider, UserReg
     // These methods are implemented for the CredentialProvider interface
     @Override
     public void onAuthenticationChallenge(Challenge challenge, CredentialCompletionHandler credentialCompletionHandler) {
-        Log.i(myTag,"Authentication Challenge received for credentials");
+        Log.i(myTag, this.getClass().getCanonicalName()+"Authentication Challenge received for credentials");
+        broadcast(AUTH_CHALLENGE_RECEIVED);
         Log.i(myTag, challenge.toString());
         UserCredential usCred = new UserCredential(userId,password,domain);
         credentialCompletionHandler.onCredentialProvided(usCred);
+
 
 
     }
 
     @Override
     public void onCredentialAccepted(Challenge challenge) {
-        Log.i(myTag, "Credentials have been accepted that were passed as authentication challenge");
+        Log.i(myTag, this.getClass().getCanonicalName()+"Credentials have been accepted that were passed as authentication challenge");
 
     }
 
     @Override
     public void onAuthenticationChallengeCancelled(Challenge challenge) {
-        Log.i(myTag, "Authentication Challenge in cancelled");
+        Log.i(myTag, this.getClass().getCanonicalName()+"Authentication Challenge in cancelled");
 
     }
     // Finished implemented  methods for the CredentialProvider interface
@@ -178,20 +229,20 @@ public class MySDKManager implements ClientListener, CredentialProvider, UserReg
 
     @Override
     public void onClientShutdown(Client client) {
-        Log.i(myTag,"onClientShutdown -->The ClientConfiguration object has been shutdown");
+        Log.i(myTag,this.getClass().getCanonicalName()+"onClientShutdown -->The ClientConfiguration object has been shutdown");
 
     }
 
     @Override
     public void onClientUserCreated(Client client, User user) {
-        Log.i(myTag, "onClientUserCreated -->The ClientConfiguration object has been created");
+        Log.i(myTag, this.getClass().getCanonicalName()+"onClientUserCreated -->The ClientConfiguration object has been created");
 
     }
 
     @Override
     public void onClientUserRemoved(Client client, User user) {
 
-        Log.i(myTag, "onClientUserRemoved -->The ClientConfiguration object has been removed");
+        Log.i(myTag, this.getClass().getCanonicalName()+"onClientUserRemoved -->The ClientConfiguration object has been removed");
 
     }
     //Finished implementing methods for the ClientListener Interface
@@ -199,18 +250,20 @@ public class MySDKManager implements ClientListener, CredentialProvider, UserReg
     //Implementing the methods for UserRegistrationListener Interface
     @Override
     public void onUserRegistrationInProgress(User user, SignalingServer signalingServer) {
-        Log.i(myTag, "User Registration in Progress");
+        Log.i(myTag, this.getClass().getCanonicalName()+"User Registration in Progress");
 
     }
 
     @Override
     public void onUserRegistrationSuccessful(User user, SignalingServer signalingServer) {
-        Log.i(myTag, "User Registration in successful");
+        Log.i(myTag, this.getClass().getCanonicalName()+"User Registration in successful");
+        broadcast(REGISTER_SUCCESS);
     }
 
     @Override
     public void onUserRegistrationFailed(User user, SignalingServer signalingServer, Exception e) {
-        Log.i(myTag, "User Registration has failed");
+        broadcast(REGISTER_FAIL);
+        Log.i(myTag, this.getClass().getCanonicalName()+"User Registration has failed");
         e.printStackTrace();
     }
 
@@ -221,12 +274,12 @@ public class MySDKManager implements ClientListener, CredentialProvider, UserReg
 
     @Override
     public void onUserAllRegistrationsFailed(User user, boolean b) {
-        Log.i(myTag, "All User Registrations have failed");
+        Log.i(myTag, this.getClass().getCanonicalName()+"All User Registrations have failed");
     }
 
     @Override
     public void onUserUnregistrationInProgress(User user, SignalingServer signalingServer) {
-        Log.i(myTag, "UNREGISTER in Progress");
+        Log.i(myTag, this.getClass().getCanonicalName()+"UNREGISTER in Progress");
     }
 
     @Override
@@ -255,7 +308,7 @@ public class MySDKManager implements ClientListener, CredentialProvider, UserReg
 
     @Override
     public void onCallRemoteAlerting(Call call, boolean b) {
-        Log.i(myTag, "Remore Call is Alerting now");
+        Log.i(myTag, "Remote Call is Alerting now");
 
     }
 
@@ -392,5 +445,20 @@ public class MySDKManager implements ClientListener, CredentialProvider, UserReg
     }
 
 
-    //Finished implementing for CallListener Interface
+    //BROADCAST METHODS
+
+    public void broadcast (String message){
+    Log.i(myTag, getClass().getCanonicalName()+"Sending a Broadcast message: "+message);
+        Intent it = new Intent();
+        it.setAction("fromSDKManager");
+        it.putExtra("message" ,message);
+        LocalBroadcastManager.getInstance(activity).sendBroadcast(it);
+
+
+
+    }
+
+
+
+
 }
